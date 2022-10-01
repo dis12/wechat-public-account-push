e26f2bbba09117093e11cf705d2a6a5cc9171937
Hongjun <96657073+dis12@users.noreply.github.com>
2022-10-01 20:13:12 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -65,7 +65,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '廖小溶',
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'odR1z6ggtUzn1REE490abuRClCp4',
+      id: 'odR1z6hmxhVATil8rJvyRxLn44Z0',
       // 你想对他发送的模板消息的模板ID
       useTemplateId: 'pD-ncdpJ0DVYDbo0aj1ty_TkbgWFVVVbPgSjtY9n91g',
       // 所在省份
